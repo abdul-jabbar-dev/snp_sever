c8655a69d402937de6f84252b8c37e92f75f0115
Abdul Jabbar <abduljabber1532002@gmail.com>
2024-03-27 03:44:59 +0600

    change cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(morganMiddleware);
 app.use(credentials);
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
