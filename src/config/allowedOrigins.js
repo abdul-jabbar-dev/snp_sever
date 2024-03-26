c312e50f5ec2893fbc2627f4d733cec1cb3c694f
Abdul Jabbar <abduljabber1532002@gmail.com>
2024-03-27 05:19:47 +0600

    add cors

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 export const allowedOrigins = [
   "http://localhost:5173",
   "*",
+  "https://660357bed72ffe1447254704--funny-semolina-3634d5.netlify.app/"
   // "https://yellow-grass-0b00f270f.5.azurestaticapps.net/"
 ];
