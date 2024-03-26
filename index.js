f893552cadd7f7e3e07def7573f1d323a4ceb61a
Abdul Jabbar <abduljabber1532002@gmail.com>
2024-03-27 05:55:05 +0600

    s

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import helmet from "helmet";
 
 import LandRoutes from "./src/routes/land.route.js";
 import UserRoutes from "./src/routes/user.route.js";
@@ -17,16 +15,10 @@ const app = express();
 
 app.use(morganMiddleware);
 app.use(credentials);
-app.use(cors({
-  origin: 'https://66035c6f8fa7e4121a116eee--funny-semolina-3634d5.netlify.app',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(helmet({
-  crossOriginResourcePolicy: false,
-}));
 
 app.use("/api/user", UserRoutes);
 
--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -3,22 +3,24 @@ import morgan from "morgan";
 
 const { combine, timestamp, json } = winston.format;
 const logger = winston.createLogger({
-    level: 'http',
-    format: combine(
-      timestamp({
-        format: 'YYYY-MM-DD hh:mm:ss.SSS A',
-      }),
-      json()
-    ),
-    transports: [new winston.transports.Console()],
-  });
-  
+  level: "http",
+  format: combine(
+    timestamp({
+      format: "YYYY-MM-DD hh:mm:ss.SSS A",
+    }),
+    json()
+  ),
+  transports: [new winston.transports.Console()],
+});
+
 export const morganMiddleware = morgan(
-    ':method :url :status :res[content-length] - :response-time ms',
-    {
-      stream: {
-        write: (message) => logger.http(message.trim()),
+  ":method :url :status :res[content-length] - :response-time ms",
+  {
+    stream: {
+      write: (message) => {
+        console.log(message);
+        return logger.http(message.trim());
       },
-    }
-  );
-  
+    },
+  }
+);
